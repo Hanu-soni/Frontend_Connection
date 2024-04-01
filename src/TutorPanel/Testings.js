b321523170b61922af353fe215396f885c27a688
Ritu Gour <69458847+ritu-gour@users.noreply.github.com>
2024-04-01 10:24:11 +0530

    create

--- src/TutorPanel/Testings.js
+++ src/TutorPanel/Testings.js
@@ -6,37 +6,7 @@ const Testings = () => {
     return (
         <div>
 
-
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1">Home</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1">Student</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1">Calendar</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1"> Online Material</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1">Expenses & Revenue</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1">Quiz</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1">Exam Features</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1">Announcements</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1"> Website</Link>
-            </li>
-            <li class="nav-item">
-                <Link to="/" className="nav-link nav-link1">Business Report</Link>
-            </li>
+            <h1>Hello</h1>
         </div>
     )
 }
